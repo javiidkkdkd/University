e413a0ded9c8e9b5816568de2933925ebff8a9cb
Javier Fuentes Hernández <168012690+javiidkkdkd@users.noreply.github.com>
2024-12-16 23:31:58 -0300

    Update Numbers.py

--- Numbers.py
+++ Numbers.py
@@ -1,46 +1,63 @@
 #Antonella Bravo y Javier Fuentes
+#funcion para leer los datos en el archivo de texto
+def lectura_datos():
+    archivo = open('en_palabras.txt', 'r', encoding='ASCII') #abre el archivo en modo lectura
+    lineas = archivo.readlines()
+    archivo.close() 
+    return [linea.strip() for linea in lineas] #devuelve una lista con las lineas sin espacios
 
 #diccionario para las palabras requeridas
-numeros = {"zero": 0, "one": 1 , "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10, "eleven": 11, "twelve": 12, "thirteen": 13, "fourteen": 14, "fifteen": 15, "sixteen": 16, "eighteen": 18, "nineteen": 19, "twenty": 20, "thirty": 30, "fourty": 40, "fifty": 50, "sixty": 60, "seventy": 70, "eighty": 80, "ninety": 90, "hundred": 100}
-
-multiplos = {"thousand": 1000, "million": 1000000}
-
-#lectura de datos
-def lectura_datos():
-    archivo = open('en_palabras.txt', 'r', encoding='UTF-8')  #abre el archivo en modo lectura
-    datos = []
-    for linea in archivo:
-        linea = linea.rstrip('\n')
-        lista = linea.split(' ')
-        datos.append(lista)
-    archivo.close()
-    return datos  
+numeros = {"zero": 0, "one": 1 , "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10, "eleven": 11, "twelve": 12, "thirteen": 13, "fourteen": 14, "fifteen": 15, "sixteen": 16, "eighteen": 18, "nineteen": 19, "twenty": 20, "thirty": 30, "fourty": 40, "fifty": 50, "sixty": 60, "seventy": 70, "eighty": 80, "ninety": 90}
+#diccionario para los multiplicadores
+multiplos = {"million": 1000000, "thousand": 1000, "hundred": 100} 
 
 #convertir palabras a numeros
-def pal_num(pal):
-    pal = pal.split()
+def pal_num(letras):
+    letras = letras.replace(',', '')
+    letras = letras.replace('-', ' ')
+    
+    letras = letras.lower()  #pasa de mayusculas a minusculas
+    letras = letras.split()  #separa las palabras
+    
     total = 0 
-    conteo = 0
+    conteo = 0 
     negative = False
     
-    for word in pal:
-        if word == "negative":
-            negative = True
-        elif word in numeros:
-            conteo += numeros[word]
-        elif word == "hundred":
-            conteo *= 100
-        elif word in multiplos:
-            total += conteo * multiplos[word]
+    for letra in letras:
+        if letra == "negative":
+            negative = True 
+        elif letra in numeros:  
+            conteo += numeros[letra] 
+        elif letra in multiplos:
+            
+            if conteo == 0:
+                conteo = 1  
+            
+            conteo *= multiplos[letra]
+        if letra == "thousand" or letra == "million":  #si encuentra estas palabras, se lo suma al total y reinicia el conteo
+            total += conteo
             conteo = 0
-
+    
     total += conteo
-    return -total if negative else total 
+    
+    if negative:
+        total = -total 
 
-def generar_salida(datos):
-    archivo2 = open("en_numeros.txt", 'w', encoding='UTF-8') #crea el un archivo .txt
+    return total
+
+#creacion del archivo
+def archivo_salida(lineas):
+    archivo2 = open('en_numeros.txt', 'w', encoding='ASCII') #crea el un archivo .txt
     archivo2.write("Antonella Bravo - Javier Fuentes\n") #integrantes
     archivo2.write("\n") #espacio en blanco
-    archivo2.write(f"{pal_num(datos)}\n") #escribe en el archivo txt los casos negativos de Yunco
-    archivo2.write("\n") 
+    for linea in lineas:
+        if linea == "": 
+            archivo2.write("\n")  
+        else:
+            numero = pal_num(linea) #separa las lineas
+            archivo2.write(f"{numero}\n") #escribe el numero de la suma 
     archivo2.close()
+
+if __name__ == "__main__":
+    lineas = lectura_datos()
+    archivo_salida(lineas)
